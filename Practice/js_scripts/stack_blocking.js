5c77a238bc7faddf0829efe24a4dd07ffdea33d5
Szymon Jankowski <63104347+SJEnvi@users.noreply.github.com>
2024-05-30 18:07:50 +0200

    Delete Practice directory

--- Practice/js_scripts/stack_blocking.js
+++
@@ -1,4 +0,0 @@
-myGreeting = setTimeout(console.log, 2000, "Hello after 2 seconds");
-// wykonaj skrypt tak jak jest
-// potem odkomentuj poniższą linijkę i sprawdź, czy coś się wyświetla w konsoli
-//while (true){;}
